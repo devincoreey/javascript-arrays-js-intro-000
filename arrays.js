2d3bbeede708bc8a149506bb6604e4b31f1ad525
devin williams <williams.devin702@gmail.com>
2020-06-10 21:38:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,4 +27,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
   return array;
 }
-function 
+function removeElementFromEndOfArray(array){
+  array.slice(0,array.length -1);
+  return array
+}
